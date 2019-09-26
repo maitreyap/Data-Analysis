f02595bb7fb2ad98fadf3614d2770e93beff9a82
maitreyapanse <mpanse@ftsinc.com>
2019-09-25 20:54:03 -0700

    Add entry_id, temperature, humidity, and soil moisture data to output csv file

--- timediff.py
+++ timediff.py
@@ -9,22 +9,34 @@ import datetime
 
 def ImportCsvData(inputcsvfilename, outputcsvfile):
     time = []
+    entry_id = []
+    temperature = []
+    humidity = []
+    SoilMoisture = []
     timediff = []
+
     with open(inputcsvfilename, 'r') as csvfile:
         csv_read = csv.reader(csvfile)
         next(csv_read)
         for line in csv_read:
             time.append(line[0])
+            entry_id.append(line[1])
+            temperature.append(line[2])
+            humidity.append(line[3])
+            SoilMoisture.append(line[4])
+
     with open(outputcsvfile, 'w') as newcsvfile:
         csv_write = csv.writer(newcsvfile)
 
-        csv_write.writerow(['Time', 'Time Diff'])
+        csv_write.writerow(['Time', 'Time Diff', 'Entry ID', 'Temperature', 'Humidity', 'SoilMoisture'])
         for x in range(1, time.__len__()):
             date_object_curr = datetime.datetime.strptime(time[x-1], '%Y-%m-%d %H:%M:%S %Z')
             date_object_next = datetime.datetime.strptime(time[x], '%Y-%m-%d %H:%M:%S %Z')
             date_object_diff = date_object_next - date_object_curr
             timediff.append(date_object_diff)
-            csv_write.writerow([date_object_curr, date_object_diff])
+
+            csv_write.writerow([date_object_curr, date_object_diff, entry_id[x-1], temperature[x-1],
+                                humidity[x-1], SoilMoisture[x-1]])
             print(time[x])
             print(date_object_diff)
 
